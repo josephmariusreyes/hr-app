20025bfac1b60cad94e8468400d9145a4dd181d5
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2025-07-06 02:31:56 +0800

    app updates

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const mainRoutes: Routes = [
-  { path: '', redirectTo: 'user/login', pathMatch: 'full' },
+  { 
+    path: '', redirectTo: 'user/login', pathMatch: 'full' 
+  },
   {
     path: 'user',
     loadChildren: () => import('../features/users/users.module').then(m => m.UsersModule)
