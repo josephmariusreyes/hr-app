9b77580c60f207f6be33a5df5533b61a58b85a3f
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2025-07-11 18:37:34 +0800

    revert back to using stand alone

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,36 +1,25 @@
-import { NgModule, isDevMode } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 
-import { AppComponent } from './app.component';
 import { mainRoutes } from './routes/app.routes';
 import { AppState } from './core/state/app.state';
 import { userReducer } from './core/state/user';
 
-@NgModule({
-  imports: [
-    BrowserModule,
-    AppComponent,
-    RouterModule.forRoot(mainRoutes),
-    StoreModule.forRoot<AppState>({ user: userReducer }),
-    EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
-    BrowserAnimationsModule,
-    // ...PrimeNG modules as needed
-  ],
-  providers: [
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    })
-    // ...other providers
-  ]
-})
-export class AppModule {}
+export const appConfig = [
+  provideRouter(mainRoutes),
+  provideStore({ user: userReducer }),
+  provideEffects([]),
+  provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  provideAnimations(),
+  providePrimeNG({
+    theme: {
+      preset: Aura
+    }
+  })
+];
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.config';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, { providers: [...appConfig] })
+  .catch((err: any) => console.error(err));
