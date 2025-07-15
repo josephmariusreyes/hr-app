a383d99b83400e154571ebe03bc48dd15cf02259
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2025-07-15 17:43:35 +0800

    add dashboard components

---
+++ src/app/features/dashboard/containers/payslips/payslips.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payslips',
+  templateUrl: './payslips.component.html',
+  styleUrls: ['./payslips.component.scss']
+})
+export class PayslipsComponent {}
--- src/app/features/dashboard/dashboard-routing.module.ts
+++ src/app/features/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardLandingComponent } from './containers/dashboard-landing/dashboard-landing.component';
+import { AttendanceComponent } from './containers/attendance/attendance.component';
+import { RequestsComponent } from './containers/requests/requests.component';
+import { PayslipsComponent } from './containers/payslips/payslips.component';
+import { ProfileComponent } from './containers/profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: DashboardLandingComponent },
+  { path: 'attendance', component: AttendanceComponent },
+  { path: 'requests', component: RequestsComponent },
+  { path: 'payslips', component: PayslipsComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
 
 @NgModule({
